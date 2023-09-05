cf974f5009f93c34687750acd1d9dfeb34fae47f
Torikul islam <toriqulislam142@gmail.com>
2023-09-05 21:48:30 +0600

    initial project setup

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar/Navbar';
+
+const Home = () => {
+    return (
+        <>
+        <Navbar />
+            <h1>this is home page</h1>
+        </>
+    );
+};
+
+export default Home;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './navbar.css'
+const Navbar = () => {
+    return (
+        <>
+            <nav>
+          
+            </nav>
+        </>
+    );
+};
+
+export default Navbar;
