d91e868c3891d1528aafb6351a8d9dc8b2de716a
Torikul islam <toriqulislam142@gmail.com>
2023-09-06 11:28:04 +0600

    navbar completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import Navbar from './Navbar/Navbar';
 
 const Home = () => {
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,34 +1,39 @@
-import React from 'react';
+
 import './navbar.css';
 import logo from '../../assets/logo.svg';
 import { BsSearch } from 'react-icons/bs';
+import { RiGlobalLine } from 'react-icons/ri'
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {FaUserCircle} from 'react-icons/fa'
 const Navbar = () => {
     return (
         <>
             <nav>
-            <div className="flex-container">
-                <div className="logo">
+                <div className="flex-container">
+                    <div className="logo">
 
-                <a href="/"><img src={logo} alt="logo" width="100" height="100"/></a>
+                        <a href="/"><img src={logo} alt="logo" width="100" height="100" /></a>
 
-                </div>
-                <div className="category">
-                    <div className="category-wrapper">
-                        <div className='anywhere'>Anywhere</div>
-                        <div className='any-week'>Any Week</div>
-                        <div className='add-guest'>Add Guests  </div>
-                        <div className='search-icon'><BsSearch /></div>
+                    </div>
+                    <div className="category">
+                        <div className="category-wrapper">
+                            <div className='anywhere'>Anywhere</div>
+                            <div className='any-week'>Any Week</div>
+                            <div className='add-guest'>Add Guests  </div>
+                            <div className='search-icon-wrapper'><BsSearch className='search-icon' /></div>
+                        </div>
+                    </div>
+                    <div className="right-side-wrapper">
+                        <div className='bnb-home-btn'><button>Airbnb your home</button></div>
+                        <div className='global-container'>
+                            <div className='global'> <RiGlobalLine className='global-icon' /></div>
+                        </div>
+                        <div className='profile-container'>
+                            <div className='account-icons'><GiHamburgerMenu className='burger-icon'/></div>
+                            <div className='account-icons'><FaUserCircle className='user-icon'/></div>
+                        </div>
                     </div>
                 </div>
-                <div className="user-profile">
-                    <div className='bnb-home-btn'><button>Airbnb your home</button></div>
-                    <div></div>
-                    <div>
-                        <div></div>
-                        <div></div>
-                    </div>
-                </div>
-            </div>
             </nav>
         </>
     );
